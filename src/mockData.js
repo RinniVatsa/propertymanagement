b5016e2b6849198769517eddadb08107b82e0498
Rinni Vatsa <rinnivatsa09@gmail.com>
2023-08-24 12:42:42 +0530

    first commit

---
+++ src/mockData.js
@@ -0,0 +1,40 @@
+export const PropertyData = [{
+        id: 1,
+        name: 'Signature Global Park',
+        address: 'Sector 38,Gurgaon',
+        tenants: [
+            { name: 'Hritik' },
+            { name: 'Abhishek' },
+        ],
+    },
+    {
+        id: 2,
+        name: 'Suncity Platinum Towers',
+        address: '619, DownTown',
+        tenants: [
+            { name: 'Piyush' },
+            { name: 'Deepika' },
+        ],
+    },
+
+
+    {
+        id: 3,
+        name: 'DLF Magnolias',
+        address: '54,sector 23,Gurgaon',
+        tenants: [
+            { name: 'Mridul' },
+            { name: 'Mehek' },
+        ],
+    },
+
+    {
+        id: 4,
+        name: 'Emaar Marbella',
+        address: '193-12 Golf Course road',
+        tenants: [
+            { name: 'Dikshita' },
+            { name: 'Ananya' },
+        ],
+    },
+];
